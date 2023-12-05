a62f39b60d99c7c632429674b316f57a44c9872d
javrin <jawabiscuit@users.noreply.github.com>
2023-12-05 14:09:54 -0500

    Formatting

--- multiSuggester.js
+++ multiSuggester.js
@@ -5,14 +5,19 @@
 
 /**
  * Spawns a multi-select suggester prompt and returns the user's chosen items.
+ * @template T The type of items in the array.
  * @param {object} tp Templater tp object.
- * @param {string[] | ((item: T) => string)} textItems Array of strings representing the text that will be displayed for each item in the suggester prompt. This can also be a function that maps an item to its text representation.
- * @param {T[]} items Array containing the values of each item in the correct order.
- * @param {boolean} throwOnCancel Throws an error if the prompt is canceled, instead of returning a null value.
- * @param {string} placeholder Placeholder string of the prompt.
- * @param {number} limit Limit the number of items rendered at once (useful to improve performance when displaying large lists).
- * @param {string} sort Sort the items in the list alphabetically ("alpha") or by occurance descending ("occurance") or by occurance ascending ("occurance|asc")
- * @returns A list of selected items.
+ * @param {string[]} textItems Array of strings or function
+ * that maps an item to its text representation for each item in the suggester prompt.
+ * @param {string[]} items Array containing values of each item in correct order.
+ * @param {boolean} throwOnCancel If true, throws error if prompt is canceled instead
+ * of returning null.
+ * @param {string} placeholder Placeholder string for the prompt.
+ * @param {number} limit Limit on number of items rendered at once (improves performance
+ * when displaying large lists).
+ * @param {string} sort Sorts list alphabetically ("alpha"), by occurrence descending
+ * ("occurrence"), or by occurrence ascending ("occurrence|asc").
+ * @return {T[]} A list of selected 'items' based on user input from suggester prompt.
  */
 async function multiSuggester(
     tp,
@@ -22,55 +27,56 @@ async function multiSuggester(
     placeholder = "",
     limit = undefined,
     sort = "",
-  ) {
-    // List of items that are selected in the suggester
-    const selectedItems = [];
-    switch(sort) {
-        // Sorted alphabetically
-        case "alpha":
-            items = items.sort( (a, b) => a[0].localeCompare(b[0]) )
-            break;
-        // Sorted by occurance
-        case "occurance":
-            items = items.sort( (a, b) => b[1] - a[1], "desc" );
-            break;
-        // Sorted by occurance ascending
-        case "occurance|asc":
-            items = items.sort( (a, b) => b[1] - a[1], "asc" );
-            break;
-    }
-    // Looping to keep suggester modal open until escape is hit
-    while (true) {
-      const selectedItem = await tp.system.suggester(
+) {
+  // List of items that are selected in the suggester
+  const selectedItems = [];
+  switch (sort) {
+    // Sorted alphabetically
+    case "alpha":
+      items = items.sort( (a, b) => a[0].localeCompare(b[0]) );
+      break;
+      // Sorted by occurance
+    case "occurance":
+      items = items.sort( (a, b) => b[1] - a[1], "desc" );
+      break;
+      // Sorted by occurance ascending
+    case "occurance|asc":
+      items = items.sort( (a, b) => b[1] - a[1], "asc" );
+      break;
+  }
+  // Looping to keep suggester modal open until escape is hit
+  while (true) {
+    const selectedItem = await tp.system.suggester(
         textItems,
         items,
         throwOnCancel,
         placeholder,
-        limit
-      );
+        limit,
+    );
       // If escape is hit, break out of loop to close suggester modal
-      if (!selectedItem) {
-        break;
-      }
-      // Hack to create a new item
-      if (selectedItem[0] === "-- New --") {
-        newTag = await tp.system.prompt("New tag");
-        selectedItems.push([`#${newTag}`, 0]);
-        continue;
-      }
-
-      // Otherwise, add selected item to list of selected items, remove item from multi-select, and keep looping
-      selectedItems.push(selectedItem);
-      const selectedItemIndex = items.findIndex((item) => item === selectedItem);
-      if (selectedItemIndex >= 0) {
-        items.splice(selectedItemIndex, 1);
-        if (Array.isArray(textItems)) {
-          textItems.splice(selectedItemIndex, 1);
-        }
-      }
+    if (!selectedItem) {
+      break;
+    }
+    // Hack to create a new item
+    if (selectedItem[0] === "-- New --") {
+      newTag = await tp.system.prompt("New tag");
+      selectedItems.push([`#${newTag}`, 0]);
+      continue;
     }
 
-    return selectedItems;
+    // Otherwise, add selected item to list of selected items, remove item from multi-
+    // select, and keep looping
+    selectedItems.push(selectedItem);
+    const selectedItemIndex = items.findIndex((item) => item === selectedItem);
+    if (selectedItemIndex >= 0) {
+      items.splice(selectedItemIndex, 1);
+      if (Array.isArray(textItems)) {
+        textItems.splice(selectedItemIndex, 1);
+      }
+    }
   }
 
-  module.exports = multiSuggester;
+  return selectedItems;
+}
+
+module.exports = multiSuggester;
